deec209b03ec769679cd5d64a307996c00534b2e
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-02-11 12:31:14 +0530

    File Added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# AnimeEpisodeThumbnails
- Collection of thumbnails for anime episodes
+## AnimeEpisodeThumbnails
+
+Collection of thumbnails of anime episodes
+
+- Automatic Generation And Saving Of Thumbnails
+
+## How Animes Will Be Added
+
+- First of all the popular animes will be added, as in https://gogoanime3.co/popular.html
---
+++ main.py
@@ -0,0 +1,88 @@
+import requests
+import os
+from Utils.ScreenshotGen import get_screenshot
+
+def getJson(url):
+    print("Getting", url)
+    i = 0
+    while i < 5:
+        i += 1
+        try:
+            r = requests.get(url)
+            data = r.json()
+            return data
+        except:
+            print("Retrying", i, url)
+            continue
+    
+    raise Exception("Failed to get json", url)
+
+def generateThumbnailsOfAnime(animeid):
+    url ='https://api.anime-dex.workers.dev/anime/' + animeid
+    data = getJson(url)['results']
+
+    episodes = data['episodes']
+    for episode in episodes:
+        episodeid = episode[1]
+
+        # checking if thumbnail already exists
+        if os.path.exists(f"./Thumbnails/{animeid}/{episodeid}.jpg"):
+            print("Skipping", episodeid)
+            continue
+        
+        ss =get_screenshot(episodeid)
+
+        # checking if anime folder exists
+        if not os.path.exists(f"./Thumbnails/{animeid}"):
+            os.makedirs(f"./Thumbnails/{animeid}")
+
+        os.rename(ss, f"./Thumbnails/{animeid}/{episodeid}.jpg")
+        print("Downloaded", episodeid)
+
+
+# generateThumbnailsOfAnime('horimiya-dub')
+        
+def updateThumbnail(episodeid):
+    animeid, episode = episodeid.split("-episode-")
+
+    ss = get_screenshot(episodeid)
+
+    # checking if anime folder exists
+    if not os.path.exists(f"./Thumbnails/{animeid}"):
+        os.makedirs(f"./Thumbnails/{animeid}")
+
+    
+    # checking if thumbnail already exists
+    if os.path.exists(f"./Thumbnails/{animeid}/{episodeid}.jpg"):
+        os.remove(f"./Thumbnails/{animeid}/{episodeid}.jpg")
+
+    os.rename(ss, f"./Thumbnails/{animeid}/{episodeid}.jpg")
+    print("Updated", episodeid)
+
+# updateThumbnail('horimiya-episode-1')
+    
+
+def getPopularAnimes(page):
+    url = 'https://api.anime-dex.workers.dev/gogoPopular/' + str(page)
+    data = getJson(url)['results']
+    
+    for anime in data:
+        try:
+            animeid = anime['id']
+            generateThumbnailsOfAnime(animeid)
+        except:
+            print("Failed to download", animeid)
+            continue
+
+
+if __name__ == '__main__':
+    with open('PopularPagesDone.txt', 'r') as f:
+        page = int(f.read())
+
+    page += 1
+    getPopularAnimes(page)
+
+    with open('PopularPagesDone.txt', 'w') as f:
+        f.write(str(page))
+
+    print("Done")
